2464af6a6707b2325f4a87972adbb1e0ae73fe05
Eduardo Oliveira <el702985@gmail.com>
2023-07-22 17:01:41 -0300

    corrigindo bug na msg dos forms

--- src/components/formRegister/index.tsx
+++ src/components/formRegister/index.tsx
@@ -85,7 +85,7 @@ export default function FormRegister() {
       redirect.push('/user/dasboard');
     } catch (err: any) {
       toast.error(
-        get(err.response, 'err.response.data', false)
+        get(err.response, 'err.response.data.message', false)
           ? err.response.data.message
           : 'internal server error 500',
         {
